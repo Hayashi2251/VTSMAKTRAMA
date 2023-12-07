c488968b360388b21efe20a5b39c6955b5925247
Siloam <56503811+Hayashi2251@users.noreply.github.com>
2023-12-07 10:38:33 +0700

    Update script.js

--- VTXI4/script.js
+++ VTXI4/script.js
@@ -8229,7 +8229,7 @@
  "class": "HotspotPanoramaOverlay",
  "areas": [
   {
-   "click": "this.openLink('https://vtsmaktrama.vercel.app/XI5_3', '_top')",
+   "click": "this.openLink('https://smaktramavt.vercel.app/XI5_3', '_top')",
    "class": "HotspotPanoramaOverlayArea",
    "mapColor": "#FF0000"
   }
@@ -8275,7 +8275,7 @@
  "class": "HotspotPanoramaOverlay",
  "areas": [
   {
-   "click": "this.openLink('https://vtsmaktrama.vercel.app/', '_top')",
+   "click": "this.openLink('https://smaktramavt.vercel.app/', '_top')",
    "class": "HotspotPanoramaOverlayArea",
    "mapColor": "#FF0000"
   }
@@ -9131,7 +9131,7 @@
  "class": "HotspotPanoramaOverlay",
  "areas": [
   {
-   "click": "this.openLink('https://vtsmaktrama.vercel.app/XI5_3', '_top')",
+   "click": "this.openLink('https://smaktramavt.vercel.app/XI5_3', '_top')",
    "class": "HotspotPanoramaOverlayArea",
    "mapColor": "#FF0000"
   }
@@ -9177,7 +9177,7 @@
  "class": "HotspotPanoramaOverlay",
  "areas": [
   {
-   "click": "this.openLink('https://vtsmaktrama.vercel.app/', '_top')",
+   "click": "this.openLink('https://smaktramavt.vercel.app/', '_top')",
    "class": "HotspotPanoramaOverlayArea",
    "mapColor": "#FF0000"
   }
